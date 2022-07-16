70381f26e6167ecc1662194ded62b16c6223a4de
decagon <godsonjonathan39@gmail.com>
2022-07-16 23:40:31 +0100

    Added DTO to project

---
+++ src/main/java/com/example/mobileappwebservices/DTO/UserDTO.java
@@ -0,0 +1,25 @@
+package com.example.mobileappwebservices.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserDTO implements Serializable {
+
+    private static final long serialVersionUID = 4865903039190150223L;
+    private long id;
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String encryptedPassword;
+    private Boolean emailVerificationStatus;
+}
